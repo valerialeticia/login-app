27f76859b69922ad0bf7f83de5059fdab110fb11
valerialunaorla <valeria.luna@orla.tech>
2024-06-25 16:27:59 -0300

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Welcome from '../pages/Welcome/Welcome';
+import SignIn from '../pages/SignIn/SignIn';
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Welcome' component={Welcome} options={{ headerShown: false }} />
+      <Stack.Screen name='SignIn' component={SignIn} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+}
